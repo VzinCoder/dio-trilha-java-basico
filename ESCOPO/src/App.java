4fe20034cbb06645e6e5a33ab83c527b8ef4f361
VzinCoder <contato.viniciusp.b@gmail.com>
2024-04-27 04:13:20 -0300

    exemplo escopo

---
+++ ESCOPO/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double saldo = 10;
+
+        if(saldo == 10){
+            double novoSaldo = 100;
+        }
+         System.out.println(novoSaldo); // erro pois a variavel novo saldo só é visivel dentro do if
+    }
+}
