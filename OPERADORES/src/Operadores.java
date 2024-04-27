9e32f7d83e813a7adde121b9c69430d017ca0c50
VzinCoder <contato.viniciusp.b@gmail.com>
2024-04-27 02:31:28 -0300

    operador de atribuição e operadores aritméticos

---
+++ OPERADORES/src/Operadores.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        // operador de atribuição
+        int idade = 22;
+        double peso = 68.5; // usamos "." para representar a parte decimal
+        char sexo = 'M'; // para char usamos aspas simples
+        boolean doadorOrgao = false; // usamos true e false para booleanos
+        Date dataNascimento = new Date(); // usamos new para instanciar uma classe
+
+        // operadores aritméticos
+
+        double soma = 10.5 + 15.7;
+        int subtração = 10 - 11;
+        int multiplicação = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3; // resto
+        double resultado = (10 * 7);
+
+    }
+}
