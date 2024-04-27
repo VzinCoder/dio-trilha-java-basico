95995096a62cc7ab29adc726133f06ba47deb127
VzinCoder <contato.viniciusp.b@gmail.com>
2024-04-27 02:19:15 -0300

    informações sobre os tipos

--- TIPOS-VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123; // ocupa 1 bytes
+        short ano = 2024; // ocupa 2 bytes
+        int cep = 562000000; // ocupa 4 bytes
+        long cpf = 12345678901L; // ocupa 8 bytes
+        float pi = 3.14F; // ocupa 4 bytes
+        double salarioMinimo = 2500; //  ocupa 8 bytes
+
+        final String MEUNOME = "Vinicius Pereira Bezerra"; // final define uma constante
+        
+        // MEUNOME = 'teste'; nao funciona pois é uma constante!
     }
 }
